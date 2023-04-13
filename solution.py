934a1243781bf8420d43f17f24572e34f668c202
gi-garif <84095383+gi-garif@users.noreply.github.com>
2023-04-13 09:08:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 33217853 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +12,24 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.07
+    
+    control_success_rate = x_success / x_cnt
+    test_success_rate = y_success / y_cnt
+    success_rate = (x_success+y_success) / (x_cnt + y_cnt)
+    
+    SE = ((success_rate*(1-success_rate))/x_cnt + (success_rate*(1-success_rate))/y_cnt)**0.5
+    
+    z = (control_success_rate - test_success_rate) / SE
+    
+    p_value = norm.sf(abs(z)) * 2
+    
+    if p_value < alpha:
+      result = True  
+    else:
+      result = False
+    
+    return result 
+
+  
+  
